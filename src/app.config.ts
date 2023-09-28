f1bfd6f263a88579c49489b25aa625cfb5ec9672
Joao Santos <info@webfueler.com>
2023-09-28 15:28:00 +0100

    refactor: db implementation cleanup

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 import { join } from "node:path";
 
 // default database file
-const DB_FILE = join(__dirname, "..", "registry.db");
+const DB_FILE = join(__dirname, "..", "db", "registry.db");
 
 // defining as constant while we don't have endpoints for these
 // they are added to database upon initialization
